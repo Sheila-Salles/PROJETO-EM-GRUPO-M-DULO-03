c4dad499f66b8260b767fc62851ec2012f40a83f
CassioRibeiro <123372841+CassioRibeiro@users.noreply.github.com>
2023-04-11 20:38:25 -0300

    Update CONSULTAS.sql
    ultima versão

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -8,14 +8,15 @@ SELECT alunos.nome, cursos.nome
  FROM alunos
  INNER JOIN alunos_cursos
  ON alunos.matricula = Alunos_Cursos.Matricula_aluno
- LEFT JOIN cursos
+ INNER JOIN cursos
  ON alunos_cursos.id_cursos = cursos.id
  ORDER BY alunos.nome
  
 --3. Selecionar quais pessoas facilitadoras atuam em mais de uma turma.
 SELECT Facilitadores.Nome, COUNT(DISTINCT Cursos_Facilitadores.ID_cursos) AS Quantidade_Turmas
 FROM Facilitadores 
-INNER JOIN Cursos_Facilitadores  ON Facilitadores.ID = Cursos_Facilitadores.ID_facilitadores
+INNER JOIN Cursos_Facilitadores 
+ON Facilitadores.ID = Cursos_Facilitadores.ID_facilitadores
 GROUP BY Facilitadores.Nome
 HAVING COUNT(DISTINCT Cursos_Facilitadores.ID_cursos) > 1;
  
@@ -28,7 +29,8 @@ WHERE endereco LIKE '%Nova Iguaçu%'
 
 SELECT Cursos.Nome, COUNT(Alunos_Cursos.Matricula_aluno) AS Num_Alunos
 FROM Cursos
-INNER JOIN Alunos_Cursos ON Cursos.ID = Alunos_Cursos.ID_cursos
+INNER JOIN Alunos_Cursos
+ON Cursos.ID = Alunos_Cursos.ID_cursos
 GROUP BY Cursos.ID
 ORDER BY Num_Alunos DESC
 LIMIT 1;
